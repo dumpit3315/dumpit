d02d4c2845923eb38a372ba8bb63c9db6d03a14f
wrapper <wrapper@pop-os.localdomain>
2024-01-17 11:25:10 +0700

    engineio

--- dumpit.py
+++ dumpit.py
@@ -1111,8 +1111,8 @@ class MainApp(main.main):
         
         self._reconnecting = True
         try:
-            self._sio.disconnect()
-            time.sleep(2)
+            self._sio.client.eio.disconnect()
+            time.sleep(2)            
             
             self._sio = socketio.SimpleClient(handle_sigint=False)
             gc.collect()
